9c12b3d4ef72df94a89f2c9590978e2f2f947de1
AVidhanR <itsvidhanreddy@gmail.com>
2024-11-24 21:55:29 +0530

    Update page title and enhance GitHub repo link styling in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center mx-auto my-2 p-2">
-      <h1 className="text-4xl my-2 font-bold mb-4">
-        Zustand On Go State Change
-      </h1>
+      <h1 className="text-4xl my-2 font-bold mb-4">Zustand OnChange State</h1>
       <input
         className="border rounded-lg w-1/2 dark:text-black border-gray-300 p-1"
         type="text"
@@ -20,9 +18,19 @@ export default function Home() {
       />
       <br />
       <h1 className="text-2xl text-center">{appName}</h1>
-      <span className="flex flex-row">
+      <span className="flex flex-row text-lg">
+        Check out the GitHub repo from&nbsp;
+        <Link
+          href={"https://github.com/AVidhanR/ZustandExp"}
+          className="hover:underline font-bold"
+          target="_blank"
+        >
+          here
+        </Link>
+      </span>
+      <span className="flex flex-row text-lg">
         Check out the Zustand Form example from&nbsp;
-        <Link href={"/zustand-form"} className="hover:underline">
+        <Link href={"/zustand-form"} className="hover:underline font-bold">
           here
         </Link>
       </span>
